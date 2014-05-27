0e18f84f3b6b5a8d52d287f6bed83c6c2d35bef2
Rusty Myers <rustymyers@gmail.com>
2014-05-27 15:57:12 -0400

    pow2.py - print all powers from 0-31 for the number 2

---
+++ rzm102/pow2.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python
+#--------------------------------------------------------------------------------------------------
+#-- pow2
+#--------------------------------------------------------------------------------------------------
+# Program    : pow2
+# To Complie : n/a
+#
+# Purpose    : Print out all powers of 2 from 2^0 through 2^31
+#
+# Called By  :
+# Calls      :
+#
+# Author     : Rusty Myers <rzm102@psu.edu>
+# Based Upon :
+#
+# Note       : 
+#
+# Revisions  : 
+#           2014-05-27 <rzm102>   Initial Version
+#
+# Version    : 1.0
+#--------------------------------------------------------------------------------------------------
+
+
+for x in xrange(0,31):
+    print "2^{0:.1f} power = {1:.3f}".format(x,(2**x))
