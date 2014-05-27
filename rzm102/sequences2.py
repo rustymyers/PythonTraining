db9620185c52b391453a2f3a544fc734baaa5dee
Rusty Myers <rustymyers@gmail.com>
2014-05-27 16:48:19 -0400

    sequence2.py - added reordering of list in two ways.

--- rzm102/sequences2.py
+++ rzm102/sequences2.py
@@ -28,3 +28,13 @@ fruits = [
 
 clean_fruits = [ fruit.lower().strip() for fruit in fruits]
 print ",".join(clean_fruits)
+
+# Re-Order clean_fruits with new list
+ordered_fruits = clean_fruits[1:]
+ordered_fruits.append(clean_fruits[0])
+print ",".join(ordered_fruits)
+
+# Re-Order clean_fruits without new list
+first_fruit = clean_fruits.pop(0)
+clean_fruits.append(first_fruit)
+print ",".join(clean_fruits)
