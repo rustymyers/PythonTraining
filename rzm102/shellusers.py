3928bdeb654b37bba006a6f1dee761499ebf872b
Rusty Myers <rustymyers@gmail.com>
2014-05-28 14:34:41 -0400

    shellusers.py - loop through passwd file and report number of shells being used

---
+++ rzm102/shellusers.py
@@ -0,0 +1,39 @@
+#!/usr/local/bin/python
+#--------------------------------------------------------------------------------------------------
+#-- shellusers.py
+#--------------------------------------------------------------------------------------------------
+# Program    : shellusers.py
+# To Complie : n/a
+#
+# Purpose    : 
+#
+# Called By  :
+# Calls      :
+#
+# Author     : Rusty Myers <rzm102@psu.edu>
+# Based Upon :
+#
+# Note       : 
+#
+# Revisions  : 
+#           2014-05-28 <rzm102>   Initial Version
+#
+# Version    : 1.0
+#--------------------------------------------------------------------------------------------------
+
+users_dict = {}
+counts = {}
+
+with open("passwd") as f:
+    for line in f:
+        (username,password,uid,gid,full_name,home,shell) = line[:-2].split(":")
+        if shell == "":
+            shell = "NONE"
+        users_dict[username] = [password,uid,gid,full_name,home,shell]
+
+for name,info_list in users_dict.iteritems():
+    shell = info_list[5]
+    counts[shell] = counts.get(shell,0) + 1
+
+for shell,count in counts.iteritems():
+    print("{0:<15}    {1:>}".format(shell,count))
