865b7180b2f9d797a378ad0c1f9204111c8355e6
Rusty Myers <rustymyers@gmail.com>
2014-05-29 13:05:17 -0400

    president_sort.py - shortened by one line, combining sort into for loop.

--- rzm102/president_sort.py
+++ rzm102/president_sort.py
@@ -29,13 +29,9 @@ with open("presidents.txt","r+") as pres_txt:
     for line in pres_txt:
         presidents.append(line[:-1].split(":"))
 
-# print presidents
-pres_sort = sorted(presidents,key=operator.itemgetter(1,2))
-
 # Print Fname, lname, Bstate.
-for president in pres_sort:
+for president in sorted(presidents,key=operator.itemgetter(1,2)):
     print("{0}, {1} - Home State: {2}".format(president[1],president[2],president[10]))
 
-# print pres_sort.sorted()
 # Pres#:LastName:FirstMiddle:BY:BM:BD:DY:DM:DD:BirthTown:BirthState:SOY:SOM:SOD:EOY:EOM:EOD:Party
 # 40:Reagan:Ronald Wilson:1911:Feb:06:2004:Jun:05:Tampico:Illinois:1981:Jan:20:1989:Jan:20:Republican
