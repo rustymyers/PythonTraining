cfcd4e9c185edb6b9b7b157ff3e58ecef5218231
Rusty Myers <rustymyers@gmail.com>
2014-05-29 14:26:38 -0400

    president_sort.py - adding padding to output

--- rzm102/president_sort.py
+++ rzm102/president_sort.py
@@ -31,7 +31,7 @@ with open("presidents.txt","r+") as pres_txt:
 
 # Print Fname, lname, Bstate.
 for president in sorted(presidents,key=operator.itemgetter(1,2)):
-    print("{0}, {1} - Home State: {2}".format(president[1],president[2],president[10]))
+    print("{1:>30} {0:<12} - Home State: {2}".format(president[1],president[2],president[10]))
 
 # Pres#:LastName:FirstMiddle:BY:BM:BD:DY:DM:DD:BirthTown:BirthState:SOY:SOM:SOD:EOY:EOM:EOD:Party
 # 40:Reagan:Ronald Wilson:1911:Feb:06:2004:Jun:05:Tampico:Illinois:1981:Jan:20:1989:Jan:20:Republican
